3e8a9e9508ebe0a7fdc2338d18401a2b749d870f
Neeraj Rekwar <88387699+neerajrekwar@users.noreply.github.com>
2023-01-21 16:41:07 +0000

    fgsd

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-const handleSubmit = (event) => {
-    event.preventDefault();
-  
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-    
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => console.log("Form successfully submitted"))
-      .catch((error) => alert(error));
-  };
-  
-  document
-    .querySelector("form")
-    .addEventListener("submit", handleSubmit);
-  
+function change() {
+  // document.getElementsByTagName("h2") returns a NodeList of the <h2>
+  // elements in the document, and the first is number 0:
+
+  const header = document.getElementsByTagName("h2").item(0);
+  // the firstChild of the header is a Text node:
+  header.firstChild.data = "A dynamic document";
+  // now the header is "A dynamic document".
+
+  const para = document.getElementsByTagName("p").item(0);
+  para.firstChild.data = "This is the first paragraph.";
+
+  // create a new Text node for the second paragraph
+  const newText = document.createTextNode("This is the second paragraph.");
+  // create a new Element to be the second paragraph
+  const newElement = document.createElement("p");
+  // put the text in the paragraph
+  newElement.appendChild(newText);
+  // and put the paragraph on the end of the document by appending it to
+  // the body (which is the parent of para)
+  para.parentNode.appendChild(newElement);
+}
